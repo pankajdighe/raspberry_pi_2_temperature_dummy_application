48979461f8a4cefd2be2bc502502c451ec8d1eb7
pankaj_dighe <pankajdighe77@gmail.com>
2016-12-02 23:11:58 -0800

    Added changes

--- app/main.py
+++ app/main.py
@@ -12,16 +12,16 @@ mqttc.loop_start()
 #read temperature
 def read_temperature_data():
 	humidity, temperature = Adafruit_DHT.read_retry(11,4)
-    return temperature
+	return temperature
 
 #publish temperature
 while 1:
-    t=read_temperature_data()
-    print "Publishing data"
-    device_uuid=os.environ['RESIN_DEVICE_UUID'];
-    print device_uuid
-    (result,mid)=mqttc.publish("topic/GeneralizedIoT/"+str(device_uuid),t,2)
-    time.sleep(1)
+	t=read_temperature_data()
+	print "Publishing data"
+	device_uuid=os.environ['RESIN_DEVICE_UUID'];
+	print device_uuid
+	(result,mid)=mqttc.publish("topic/GeneralizedIoT/"+str(device_uuid),t,2)
+	time.sleep(1)
 
 mqttc.loop_stop()
 mqttc.disconnect()
